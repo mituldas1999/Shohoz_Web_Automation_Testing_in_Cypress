540716e7f792764b7aca304bedccb9f311e1a342
Mitul das <mituldas751@gmail.com>
2023-12-26 16:09:19 +0600

    update

--- cypress/e2e/POM/BusTest.cy.js
+++ cypress/e2e/POM/BusTest.cy.js
@@ -1,11 +1,10 @@
 import { Bus } from "../../pages/Bus";
-
 describe('Bus_Test', () =>{
 
     const bus=new Bus
     it("Bus", () =>{
         bus.visit()
-
+        bus.Click_Bus().click()
     })
 
 
--- cypress/pages/Bus.js
+++ cypress/pages/Bus.js
@@ -4,10 +4,7 @@ export class Bus{
         cy.visit('https://www.shohoz.com/')
         cy.title().should('be.equal',"Bangladesh's Largest Online Ticket Destination | Shohoz")
     }
-    Form(){
-        
+    Click_Bus(){
+        return cy.get('ol > :nth-child(1) > a')
     }
-
-
-
 }
