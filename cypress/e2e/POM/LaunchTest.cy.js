ec5fb7a4e2eaa182bd7ea2050748534e91c1bb44
Mitul Das <59419400+mituldas1999@users.noreply.github.com>
2023-12-29 00:34:51 +0600

    update launch

--- cypress/e2e/POM/LaunchTest.cy.js
+++ cypress/e2e/POM/LaunchTest.cy.js
@@ -3,11 +3,9 @@ import { Launch } from "../../pages/Launch"
 describe('Launch_Test', () =>{
 
     const launch=new Launch
-    it("Launch", () =>{
+    it("inputForm", () =>{
         launch.visit()
-        // cy.wait(10000)
-        // launch.clickLaunch().click()
-        cy.get("ol > :nth-child(2) > a").click()
-        cy.get("#dest_from").type("Hello")
+        launch.clickLaunch().click()
+        launch.inputData()
     })
 })
--- cypress/pages/Launch.js
+++ cypress/pages/Launch.js
@@ -7,4 +7,15 @@ export class Launch{
     clickLaunch(){
         return cy.get('ol > :nth-child(2) > a')
     }
+    inputData(){
+        cy.get("#dest_from").type("Dhaka")
+        cy.get('#ui-id-3').click()
+        cy.get('#dest_to').type('Barisal')
+        cy.get('#ui-id-4').click()
+        // cy.get('#ui-datepicker-div > div > a.ui-datepicker-next.ui-corner-all > span').click()
+        cy.get('#ui-datepicker-div > table > tbody > tr:nth-child(6) > td:nth-child(1) > a').click()
+        cy.get('#launchsearch > ul > div:nth-child(6) > div > button').click({force: true})
+        cy.get('#16257').click();
+
+    }
 }
